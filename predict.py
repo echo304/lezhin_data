723cb465a4a13ac4b4b8016c18e547087e82a08a
Sangboak Lee <echo3042@gmail.com>
2017-08-19 22:44:43 +0900

    refactoring

--- predict.py
+++ predict.py
@@ -10,20 +10,24 @@ def replace_hash_with_int(hash, hash_list):
   index = np.where(hash_list == hash)[0][0]
   return index
 
+def process_hashed_value(df):
+  unique_value_7 = df[7].unique()
+  unique_value_8 = df[8].unique()
+  unique_value_16 = df[16].unique()
+  unique_value_18 = df[18].unique()
+  df[7] = df[7].map(lambda x: replace_hash_with_int(x, unique_value_7))
+  df[8] = df[8].map(lambda x: replace_hash_with_int(x, unique_value_8))
+  df[16] = df[16].map(lambda x: replace_hash_with_int(x, unique_value_16))
+  df[18] = df[18].map(lambda x: replace_hash_with_int(x, unique_value_18))
+  return df
+
 df = pd.read_table("./lezhin_public_dataset_training.tsv", header=None)
 
-# # randomly shuffle dataset
-df = df.sample(frac=1).reset_index(drop=True)
-
-# Process hashed value 
-unique_value_7 = df[7].unique()
-unique_value_8 = df[8].unique()
-unique_value_16 = df[16].unique()
-unique_value_18 = df[18].unique()
-df[7] = df[7].map(lambda x: replace_hash_with_int(x, unique_value_7))
-df[8] = df[8].map(lambda x: replace_hash_with_int(x, unique_value_8))
-df[16] = df[16].map(lambda x: replace_hash_with_int(x, unique_value_16))
-df[18] = df[18].map(lambda x: replace_hash_with_int(x, unique_value_18))
+# Process or Drop hashed value
+# Select either one of two
+#
+# df = process_hashed_value(df)
+df = df.drop([7, 8, 16, 18], axis=1)
 
 # convert dtype of the values
 df = df.astype(np.float64)
@@ -34,16 +38,13 @@ y = df.pop(0).values
 # example
 X = df.values
 
-scaler = preprocessing.StandardScaler().fit(X)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# split training set and test set by 8:2
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-
-X_train, y_train = scaler.transform(X_train), y_train
-X_test, y_test = scaler.transform(X_test), y_test
-
-print(X_test)
-print(X_train.shape)
-print(X_test.shape)
+# Standardization with X_train dataset
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train) 
+X_test = scaler.transform(X_test) 
 
 # no. of input feature
 input = X.shape[1]
@@ -58,7 +59,7 @@ output = 2
 alpha = 0.1
 
 # selected learning rate value
-rate = 0.005
+rate = 0.001
 
 # Prediction model
 clf = MLPClassifier(solver='adam', alpha=alpha, hidden_layer_sizes=(hidden,), 
